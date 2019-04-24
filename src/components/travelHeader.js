9f34d1dda43834a796284e55f95806bd54d1f4e6
ktichmann <ktichmann@gmail.com>
2019-04-24 22:54:52 +0200

    update travel page

--- src/components/travelHeader.js
+++ src/components/travelHeader.js
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types"
 import React from "react"
 import "../styles/travelHeader.css"
 
@@ -6,16 +5,26 @@ const TravelHeader = () => {
   return (
     <nav className="d-flex position-fixed flex-row justify-content-between">
       <div className="d-flex flex-row">
-        <div className="logo">bOnline</div>
-        <ul className="d-flex flex-row list-group-flush">
-          <li className="list-group-item">Flights</li>
-          <li className="list-group-item">Parking</li>
-          <li className="list-group-item">Transport</li>
-          <li className="list-group-item">At the Airport</li>
-          <li className="list-group-item">Help</li>
+        <div className="logo mt-4">bOnline</div>
+        <ul className="d-flex flex-row">
+          <li className="list-group-item border-0">
+            <a href="#">Flights</a>
+          </li>
+          <li className="list-group-item border-0">
+            <a href="#">Parking</a>
+          </li>
+          <li className="list-group-item border-0">
+            <a href="#">Transport</a>
+          </li>
+          <li className="list-group-item border-0">
+            <a href="#">At the Airport</a>
+          </li>
+          <li className="list-group-item border-0">
+            <a href="#">Help</a>
+          </li>
         </ul>
       </div>
-      <div>
+      <div className="right-section p-4">
         <div className="booking">My Booking</div>
         <div className="search" />
       </div>
--- src/pages/travel.js
+++ src/pages/travel.js
@@ -1,11 +1,23 @@
 import React from "react"
 import Layout from "../components/layout"
+import "../styles/travel.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 const Travel = () => {
   return (
     <Layout site="travel">
-      <h1 />
+      <div className="travel-header">
+        <h1>
+          <span class="white-bg p-2">New York</span> <br />
+          <span class="white-bg p-2">International Airport</span>
+        </h1>
+      </div>
+      <div className="travel-selection">
+        <div className="travel-selection__header">
+          <span>VIEW DEPARTURES</span>
+          <span>VIEW ARRIVALS</span>
+        </div>
+      </div>
     </Layout>
   )
 }
