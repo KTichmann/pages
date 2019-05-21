654dfb695e97f976f707f10e3d80731b0499fad9
unknown <ktichmann@gmail.com>
2019-05-21 17:52:13 +0200

    add/update landing-page

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -1,9 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import laptop from "../images/laptop.png"
+import "../styles/landing-page.css"
+import card from "../images/card.png"
+import coins from "../images/coins.png"
+import refund from "../images/refund.png"
+import logoOne from "../images/logo-1.png"
+import logoTwo from "../images/logo-2.png"
+import logoThree from "../images/logo-3.png"
+import logoFour from "../images/logo-4.png"
 
 const IndexPage = () => (
   <div>
@@ -17,7 +21,7 @@ const IndexPage = () => (
       <h1 style={{ fontSize: "3rem", fontWeight: 600 }} className="mb-3">
         Sell online with us
       </h1>
-      <h3 className="mb-4">Trusted by everyone, hated by noone</h3>
+      <h3 className="mb-4">Trusted by everyone, hated by none</h3>
       <div className="freeTrial d-flex justify-content-center mb-1">
         <input
           style={{ width: "20rem" }}
@@ -25,113 +29,59 @@ const IndexPage = () => (
           type="text"
           placeholder="enter your email address"
         />
-        <button className="btn" style={{ backgroundColor: "rgb(4,4,4)" }}>
+        <button
+          className="btn"
+          style={{ backgroundColor: "rgb(4,4,4)", color: "white" }}
+        >
           Start a free trial
         </button>
       </div>
-      <small style={{ display: "block" }} className="mb-4">
+      <small style={{ display: "block", color: "white" }} className="mb-4 mt-3">
         Try our services for 14 days for free. No credit card required.
       </small>
-      <img src="" alt="laptop" />
+      <img src={laptop} alt="laptop" className="laptop" />
+      <div class="curve">
+        <svg viewBox="0 0 50 50" preserveAspectRatio="xMinYMin meet">
+          <path
+            d="M0,10 C15,20 35,0 50,10 L50,00 L0,0 Z"
+            style={{ stroke: "none", fill: "#5c6ac4" }}
+          />
+        </svg>
+      </div>
     </div>
-    <div className="products container">
+    <div className="products container pt-3">
       <div className="row">
         <div className="col-md-4">
-          <img src="" alt="icon" />
-          <h6>Lorem ipsum dolor</h6>
+          <img src={card} className="icon" alt="icon" />
+          <h6 className="bold">Lorem ipsum dolor</h6>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-            quia totam dolor aliquam, molestias quos nam alias odio mollitia
-            dolorum officiis omnis nesciunt quae ipsa id molestiae possimus
-            necessitatibus. Temporibus.
+            quia totam dolor aliquam, molestias.
           </p>
         </div>
         <div className="col-md-4">
-          <img src="" alt="icon" />
-          <h6>Lorem ipsum dolor</h6>
+          <img src={coins} className="icon" alt="icon" />
+          <h6 className="bold">Lorem ipsum dolor</h6>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-            quia totam dolor aliquam, molestias quos nam alias odio mollitia
-            dolorum officiis omnis nesciunt quae ipsa id molestiae possimus
-            necessitatibus. Temporibus.
+            quia totam dolor aliquam.
           </p>
         </div>
         <div className="col-md-4">
-          <img src="" alt="icon" />
-          <h6>Lorem ipsum dolor</h6>
+          <img src={refund} className="icon" alt="icon" />
+          <h6 className="bold">Lorem ipsum dolor</h6>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-            quia totam dolor aliquam, molestias quos nam alias odio mollitia
-            dolorum officiis omnis nesciunt quae ipsa id molestiae possimus
-            necessitatibus. Temporibus.
+            quia totam dolor aliquam, molestias quos nam.
           </p>
         </div>
       </div>
     </div>
     <div className="partners d-flex justify-space-between">
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" />
-    </div>
-    <div className="reasons">
-      <h4>Reasons to choose us</h4>
-      <div className="reason">
-        <img src="" alt="" />
-        <div className="text">
-          <h6>Reason One</h6>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            necessitatibus eius facilis, officia ipsum accusantium tenetur
-            labore error veniam
-          </p>
-        </div>
-      </div>
-      <div className="reason">
-        <img src="" alt="" />
-        <div className="text">
-          <h6>Reason Two</h6>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            necessitatibus eius facilis, officia ipsum accusantium tenetur
-            labore error veniam
-          </p>
-        </div>
-      </div>
-      <div className="reason">
-        <img src="" alt="" />
-        <div className="text">
-          <h6>Reason Three</h6>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            necessitatibus eius facilis, officia ipsum accusantium tenetur
-            labore error veniam
-          </p>
-        </div>
-      </div>
-      <div className="reason">
-        <img src="" alt="" />
-        <div className="text">
-          <h6>Reason Four</h6>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            necessitatibus eius facilis, officia ipsum accusantium tenetur
-            labore error veniam
-          </p>
-        </div>
-      </div>
-      <div className="reason">
-        <img src="" alt="" />
-        <div className="text">
-          <h6>Reason Five</h6>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam
-            necessitatibus eius facilis, officia ipsum accusantium tenetur
-            labore error veniam
-          </p>
-        </div>
-      </div>
+      <img src={logoOne} alt="logoOne" />
+      <img src={logoTwo} alt="logoTwo" />
+      <img src={logoThree} alt="logoThree" />
+      <img src={logoFour} alt="logoFour" />
     </div>
     <div className="quote">
       <h6>
